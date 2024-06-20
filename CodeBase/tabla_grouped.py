5b12b5c05e493b5347cf1fc76facf4ec530b19c8
Ricardo López Cruz <230110088@itsoeh.edu.mx>
2024-06-20 12:37:34 -0600

    tabla

--- CodeBase/tabla_grouped.py
+++ CodeBase/tabla_grouped.py
@@ -1,6 +1,6 @@
-def tabla_grouped(clases, lim_inf, lim_sup, mrks):
-    print("{:^10}".format("Clase"), "{:^20}".format("Limite Inferior"), "{:^20}".format("Limite Superior"), "{:^15}".format("Marca de clase"))
-    print("----------", "-------------------", "-------------------", "---------------")
+def tabla_grouped(clases, lim_inf, lim_sup, mrks, fa, fr, facum):
+    print("{:^10}".format("Clase"), "{:^25}".format("Limite Inferior"), "{:^25}".format("Limite Superior"), "{:^20}".format("Marca de clase"), "{:^20}".format("Frecuencia Absoluta"), "{:^25}".format("Frecuencia Relativa"), "{:^25}".format("Frecuencia Acumulada"))
+    print("----------", "-------------------------", "-------------------------", "-------------------", "-------------------", "-------------------------", "-------------------------")
     
     for i in range(len(clases)):
-        print("{:^10}".format(f"Clase {clases[i]}"), "{:^20.3f}".format(lim_inf[i]), "{:^20.3f}".format(lim_sup[i]), "{:^15.3f}".format(mrks[i]))
+        print("{:^10}".format(f"Clase {clases[i]}"), "{:^25.3f}".format(lim_inf[i]), "{:^25.3f}".format(lim_sup[i]), "{:^20.3f}".format(mrks[i]), "{:^20}".format(fa[i]), "{:^25.3f}".format(fr[i]), "{:^25.3f}".format(facum[i]))
