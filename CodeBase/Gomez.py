5961ec85e72ad916aa4a5b897885ecbc6f51d95e
Ricardo López Cruz <230110088@itsoeh.edu.mx>
2024-06-20 11:49:27 -0600

    gomez

--- CodeBase/Gomez.py
+++ CodeBase/Gomez.py
@@ -1,19 +1,21 @@
 import math
 
 def clases_grouped(datos):
+    rango = max(datos) - min(datos)
+    clases = 1 + 3.3 * (math.log(len(datos))/2)
+    clases_redondear = round(clases)-1
+    ancho = rango / clases_redondear
     
-    
-    rango = max(datos)-min(datos)
-    
-    clases = 1 + 3.3 * math.log(len(datos))
-    
-    clases_redondear = round(clases)
-    
-    ancho = rango/clases_redondear
-    
-    lim_inf = []
+    lim_inf = [min(datos)]
     lim_sup = []
     mrks = []
     
-    for elementos in clases:
-        lim_inf.append()
+    for i in range(clases_redondear - 1):
+        lim_inf.append(lim_inf[-1] + ancho)
+        lim_sup.append(lim_inf[-1] + ancho)
+        mrks.append((lim_inf[-1] + lim_sup[-1]) / 2)
+    
+    lim_sup.append(max(datos))
+    mrks.append((lim_sup[-1] + lim_inf[-1]) / 2)
+    
+    return lim_inf, lim_sup, mrks
